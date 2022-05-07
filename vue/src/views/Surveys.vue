ec3779d6408cbba4bcd3700f00213ffbb8bcc7c9
AndreGilles <agletank@gmail.com>
2022-05-06 20:50:11 -0400

    Setup Default Layout

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,8 +1,9 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
 <div>
     <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
       </div>
     </header>
     <main>
@@ -17,12 +18,8 @@
 </div>
 </template>
 
-<script>
-export default {
-    name: "Dashboard"
-}
+<script >
+
+export default{}
+
 </script>
-
-<style scoped>
-
-</style>
